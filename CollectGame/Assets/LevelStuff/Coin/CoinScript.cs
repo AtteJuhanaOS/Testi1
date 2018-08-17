bc38c8779862ef57c6c45d41be0428bf52a0c7e9
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2018-08-17 13:40:02 +0300

    kjik

---
+++ CollectGame/Assets/LevelStuff/Coin/CoinScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+    //CoinAmount
+    public static int CoinCount = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        ++CoinScript.CoinCount;
+		
+	}
+
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        --CoinScript.CoinCount;
+
+        if(CoinCount <= 0)
+        {
+            //victory
+            //Destroy timer and launch fireworks
+            GameObject Timer = GameObject.Find("LevelTimer");
+            Destroy(Timer);
+
+            GameObject[] FireworkSystems = GameObject.FindGameObjectsWithTag("Firework");
+            foreach (GameObject GO in FireworkSystems)
+            {
+                GO.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CollectGame/Assets/LevelStuff/Timer/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    //LevelTime
+    public float MaxTime = 60f;
+
+    [SerializeField]
+    private float CountDown = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        CountDown = MaxTime;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CountDown -= Time.deltaTime;
+
+        //Restart level if time runs out
+        if(CountDown <= 0)
+        {
+            //Reset coin count
+            CoinScript.CoinCount = 0;
+            SceneManager.LoadScene("SampleScene");
+        }
+		
+	}
+}
